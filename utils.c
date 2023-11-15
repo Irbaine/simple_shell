4e5aa5981da4e1c0c32127d23fb9d21f4d47f3ca
Ruyodan <salvator.hamdoune@gmail.com>
2023-11-15 12:36:40 -0800

    Updated

--- utils.c
+++ utils.c
@@ -1,30 +1,31 @@
 #include "main.h"
 int _strlength(char *n)
 {
-  int length = 0;
-  while (n[length])
-  {
-    length++;
-  }
-  return (length);
+int length = 0;
+while (n[length])
+{
+length++;
+}
+return (length);
 }
 
 char *_strcopy(char *dest, char *source)
 {
-  char *n = dest;
-  while (*source)
-  {
-    *n = *source;
-    n++;
-    source++;
-  }
-  *n = '\0';
-  return (dest);
+char *n = dest;
+while (*source)
+{
+*n = *source;
+n++;
+source++;
+}
+*n = '\0';
+return (dest);
 }
 
 int strcompar(char *one, char *two)
 {
 	int compar;
+
 	compar = (int)*one - (int)*two;
 	while (*one)
 	{
@@ -37,9 +38,10 @@ int strcompar(char *one, char *two)
 	return (compar);
 }
 
-void freeaos (char **arr)
+void freeaos(char **arr)
 {
 	int n;
+
 	if (!arr)
 		return;
 	for (n = 0; arr[n]; n++)
